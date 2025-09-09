e93462d8f7d6b31ff0e2540cfbd9d2016a1467f4
João M. Campagnolo <jota.campagnolo@gmail.com>
2025-09-08 23:55:10 -0300

    Adicionados os scripts `lint:check` e `lint:fix`.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Frase bonita e impactante para que alguém veja e fique extremamente feliz! 😃</h1>
+  return (
+    <h1>
+      Frase bonita e impactante para que alguém veja e fique extremamente feliz!
+      😃
+    </h1>
+  );
 }
 
 export default Home;
